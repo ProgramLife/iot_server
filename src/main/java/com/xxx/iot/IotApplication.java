60484f3d8004e4071498b7e145303246d2eec1b4
cdp <2863354573@qq.com>
2019-05-27 11:43:19 +0800

    完善启动项

--- src/main/java/com/xxx/iot/IotApplication.java
+++ src/main/java/com/xxx/iot/IotApplication.java
@@ -1,13 +1,17 @@
 package com.xxx.iot;
 
+import com.xxx.iot.tcp.TcpServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class IotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IotApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(IotApplication.class, args);
+        TcpServer tcpServer = context.getBean(TcpServer.class);
+        tcpServer.startTcp();
     }
 
 }
